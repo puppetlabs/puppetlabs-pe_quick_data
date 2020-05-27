3fc23d5f2c486f9786db7d44f681b9bba8281316
Jerry Mozes <jerry@puppet.com>
2020-05-27 16:10:54 -0600

    fix zipping and delete folders

--- tasks/zippedata.sh
+++ tasks/zippedata.sh
@@ -5,11 +5,11 @@ then
     count=$(ls -1 "$PT_output_dir"/*.gz 2>/dev/null | wc -l)
     if [ $count != 0 ]
     then
-        gzfile=$(ls -t "$PT_output_dir" | head -n1)
-        #Set the path to the gzip file we are going to use to add this data to
-        output_gz_file="$PT_output_dir"
-        output_gz_file+="/"
-        output_gz_file+="$gzfile"
+        gzfile=$(ls -t "$PT_output_dir"/*.gz | head -n1)
+        # #Set the path to the gzip file we are going to use to add this data to
+        # output_gz_file="$PT_output_dir"
+        # output_gz_file+="/"
+        # output_gz_file+="$gzfile"
     else
         echo "No gzip files available to use for adding node data"
         exit
@@ -19,15 +19,24 @@ else
     exit
 fi
 
-gzip -d "${output_gz_file}"
+echo ${gzfile}
 
-base_tarfile=$(basename $output_gz_file .gz)
+gzip -d "${gzfile}"
+
+base_tarfile=$(basename $gzfile .gz)
+
+echo $base_tarfile
 
 new_tarfile="$PT_output_dir"
 new_tarfile+="/"
 new_tarfile+="$base_tarfile"
 
-cd "${PT_output_dir}"
+echo $new_tarfile
 
-tar -rf "${new_tarfile}" "pe_roles_and_profiles"
+cd "${PT_output_dir}"
+currentdirs=$(ls -d */)
+
+tar -rf "${new_tarfile}" $currentdirs
 gzip "${new_tarfile}"
+
+rm -rf $currentdirs
