6bf2517fbd3c743fa4c07ccdb03e432a9e2919e5
Jerry Mozes <jerry@puppet.com>
2020-05-29 13:31:19 -0600

    remove upload of common.sh

--- plans/data_collect.pp
+++ plans/data_collect.pp
@@ -2,7 +2,6 @@ plan pe_quick_data::data_collect (
   TargetSpec        $targets,
   Optional[String]  $output_dir = '/var/tmp',
 ) {
-    upload_file('pe_quick_data/common.sh', "${output_dir}/common.sh", $targets)
     run_task('pe_quick_data::collect', $targets, output_dir => $output_dir)
     run_task('pe_quick_data::node_count', $targets, output_dir => $output_dir)
     run_task('pe_quick_data::resources', $targets, output_dir => $output_dir)
