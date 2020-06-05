a270e7b1e0221b0505bf2ed654a1e3c5b27deb27
Jerry Mozes <jerry@puppet.com>
2020-06-05 11:02:52 -0600

    return the result from the zipppedata task

--- plans/data_collect.pp
+++ plans/data_collect.pp
@@ -6,5 +6,5 @@ plan pe_quick_data::data_collect (
     run_task('pe_quick_data::node_count', $targets, output_dir => $output_dir)
     run_task('pe_quick_data::resources', $targets, output_dir => $output_dir)
     run_task('pe_quick_data::roles_and_profiles', $targets, output_dir => $output_dir)
-    run_task('pe_quick_data::zippedata', $targets, output_dir => $output_dir)
+    return run_task('pe_quick_data::zippedata', $targets, output_dir => $output_dir)
   }
