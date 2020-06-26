154ddc2480a0a5a8f0bb845a26bf6cd5ff35f27b
Jerry Mozes <jerry@puppet.com>
2020-06-26 14:37:53 -0600

    Add peversion info run puppet queries

--- tasks/node_count.sh
+++ tasks/node_count.sh
@@ -28,7 +28,6 @@ then
     fi
 else
     echo "No $PT_output_dir directory exists to dump files"
-    exit
 fi
 
 # See if there is already a pe_nodes directory in the output directory and if not let's create it
@@ -37,6 +36,10 @@ then
     mkdir -p "$output_dir/pe_nodes/"
 fi
 
+peversion=$(cat /opt/puppetlabs/server/pe_version)
+
+echo $peversion
+
 nodecount_file="$output_dir/pe_nodes/activenodecount.json"
 nodecountenv_file="$output_dir/pe_nodes/nodecount_byenv.json"
 nixcount_file="$output_dir/pe_nodes/nixnodecount.json"
@@ -44,27 +47,42 @@ nixosinfo_file="$output_dir/pe_nodes/nixosinfo.json"
 wincount_file="$output_dir/pe_nodes/winnodecount.json"
 winosinfo_file="$output_dir/pe_nodes/winosinfo.json"
 
-# Ensure pathing is set to be able to run puppet commands
-[[ $PATH =~ "/opt/puppetlabs/bin" ]] || export PATH="/opt/puppetlabs/bin:${PATH}"
+if [[ $peversion = *"2019"* ]]
+then
+    # Ensure pathing is set to be able to run puppet commands
+    [[ $PATH =~ "/opt/puppetlabs/bin" ]] || export PATH="/opt/puppetlabs/bin:${PATH}"
 
-echo " ** Collecting Output of: Number of Total Nodes"
-echo ""
+    echo " ** Collecting Output of: Number of Total Nodes"
+    echo ""
 
-# Getting all nodes listed in the database that are active and count them.
-# Get the count of active nodes by environment 
-puppet query "nodes [count()] {node_state = 'active'}" > $nodecount_file
-puppet query "nodes [facts_environment, count()]{node_state = 'active' group by facts_environment }" > $nodecountenv_file
+    # Getting all nodes listed in the database that are active and count them.
+    # Get the count of active nodes by environment 
+    puppet query "nodes [count()] {node_state = 'active'}" > $nodecount_file
+    puppet query "nodes [facts_environment, count()]{node_state = 'active' group by facts_environment }" > $nodecountenv_file
 
-echo " ** Collecting Output of: Number and Type of Total Linux Nodes"
-echo ""
+    echo " ** Collecting Output of: Number and Type of Total Linux Nodes"
+    echo ""
 
-# Count all nodes by Linux OS and get node name and OS version
-puppet query "inventory [count()] { facts.kernel = 'Linux' and facts.aio_agent_build is not null}" > $nixcount_file
-puppet query "inventory [certname, facts.os.name, facts.os.release.major] {facts.kernel = 'Linux' and facts.aio_agent_build is not null}" > $nixosinfo_file
+    # Count all nodes by Linux OS and get node name and OS version
+    puppet query "inventory [count()] { facts.kernel = 'Linux' and facts.aio_agent_build is not null}" > $nixcount_file
+    puppet query "inventory [certname, facts.os.name, facts.os.release.major] {facts.kernel = 'Linux' and facts.aio_agent_build is not null}" > $nixosinfo_file
 
-echo " ** Collecting Output of: Number and Type of Total Windows Nodes"
-echo ""
+    echo " ** Collecting Output of: Number and Type of Total Windows Nodes"
+    echo ""
 
-# Count all nodes by Windows OS and get node name and OS version
-puppet query "inventory [count()] { facts.kernel = 'windows' and facts.aio_agent_build is not null}" > $wincount_file
-puppet query "inventory[certname, facts.os.windows.product_name] {facts.kernel = 'windows' and facts.aio_agent_build is not null}" > $winosinfo_file
+    # Count all nodes by Windows OS and get node name and OS version
+    puppet query "inventory [count()] { facts.kernel = 'windows' and facts.aio_agent_build is not null}" > $wincount_file
+    puppet query "inventory[certname, facts.os.windows.product_name] {facts.kernel = 'windows' and facts.aio_agent_build is not null}" > $winosinfo_file
+elif [[ $peversion = *"2018"* ]]
+then
+    echo "version of PE is $peversion"
+    # Getting all nodes listed in the database that are active and count them.
+    # Get the count of active nodes by environment 
+    puppet query "nodes [count()] {node_state = 'active'}" > $nodecount_file
+    puppet query "nodes [facts_environment, count()]{node_state = 'active' group by facts_environment }" > $nodecountenv_file
+elif [[ $peversion = *"2017"* ]]
+then
+    echo "Puppet Query unavailable in $peversion" > $nodecount_file
+else
+    echo "Wrong version of PE for this task"
+fi
