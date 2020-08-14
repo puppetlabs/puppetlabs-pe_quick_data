1ba64e73e4c4f654acb5b7e03de67cd218946928
Jerry Mozes <jerry@puppet.com>
2020-08-14 13:30:52 -0600

    Comment out roles and profiles from run_task

--- plans/data_collect.pp
+++ plans/data_collect.pp
@@ -7,7 +7,7 @@ plan pe_quick_data::data_collect (
       run_task('pe_quick_data::collect', $targets, output_dir => $output_dir, enable_logs => $enable_logs, '_catch_errors' => true)
       run_task('pe_quick_data::node_count', $targets, output_dir => $output_dir, '_catch_errors' => true)
       run_task('pe_quick_data::resources', $targets, output_dir => $output_dir)
-      run_task('pe_quick_data::roles_and_profiles', $targets, output_dir => $output_dir)
+      # run_task('pe_quick_data::roles_and_profiles', $targets, output_dir => $output_dir)
       run_task('pe_quick_data::site_modules', $targets, output_dir => $output_dir)
       return run_task('pe_quick_data::zippedata', $targets, output_dir => $output_dir)
     }
